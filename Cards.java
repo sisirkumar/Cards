6c0eaf9926fa8e181562e5118b42681d77b2a128
Anusha <nanushabala@gmail.com>
2016-07-08 10:35:16 +0530

    added functions

--- Cards.java
+++ Cards.java
@@ -2,17 +2,25 @@ import java.util.Arrays;
 public class Cards {
 	    String Card1;
 	    String Card2;
+String Card3;
+String Card4;
+String Card5;
+
 	    String deck[] = new String[100];
     	String[] suits = {"C","D","H","S"};
     	String[] ranks ={"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
-	    public Cards(String c1,String c2) {
+	    public Cards(String c1,String c2,String c3,String c4,String c5) {
 	    	Card1 = c1;
 	    	Card2 = c2;
+		Card3 = c3;
+		Card4 = c4;
+		Card5 = c5;
 	    }
 	    public String compare() {
 	    	for(int i=0;i<52;i++){
 	    		deck[i]=suits[i/13]+ranks[i%13];
 	    	}
+
 	    	if(Arrays.asList(deck).indexOf(Card1)>Arrays.asList(deck).indexOf(Card2))
 	    		return deck[Arrays.asList(deck).indexOf(Card1)];
 	    	else
@@ -23,19 +31,24 @@ public class Cards {
 	    	int ranks[] = new int[10];
 	    	ranks[0]=Arrays.asList(deck).indexOf(Card1);
 	    	ranks[1]=Arrays.asList(deck).indexOf(Card2);
+		ranks[2]=Arrays.asList(deck).indexOf(Card3);
+		ranks[3]=Arrays.asList(deck).indexOf(Card4);
+		ranks[4]=Arrays.asList(deck).indexOf(Card5);
 	    	return ranks;
 	    }
 	    public int s(int[] straight){
 	    	int c = 0;
 	    	Arrays.sort(straight);
-	    	for(int i=0;i<1;i++){
+	    	for(int i=0;i<4;i++){
 	    		if(straight[i]==straight[i+1]-1)
 	    			c++;
 	    	}
-	    	if(c==1)
+	    	if(c==5)
 	    		return 1;
 	    	return 0;
 	    }
 
 }
 
+
+ 
