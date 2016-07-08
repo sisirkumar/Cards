ec22851bec615bdc3e133e5b46333040b848fd9e
sisirkumar <sisirkumar.mosali@cdk.com>
2016-07-08 09:51:00 +0530

    added Card class

--- Cards.java
+++ Cards.java
@@ -2,22 +2,39 @@ import java.util.Arrays;
 public class Cards {
 	    String Card1;
 	    String Card2;
+	    String deck[] = new String[100];
+    	String[] suits = {"C","D","H","S"};
+    	String[] ranks ={"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
 	    public Cards(String c1,String c2) {
 	    	Card1 = c1;
 	    	Card2 = c2;
 	    }
 	    public String compare() {
-	    	String dech[] = new String[100];
-	    	String[] suits = {"C","D","H","S"};
-	    	String[] pips ={"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
 	    	for(int i=0;i<52;i++){
-	    		dech[i]=suits[i/13]+pips[i%13];
+	    		deck[i]=suits[i/13]+ranks[i%13];
 	    	}
-	    	if(Arrays.asList(dech).indexOf(Card1)>Arrays.asList(dech).indexOf(Card2))
-	    		return dech[Arrays.asList(dech).indexOf(Card1)];
+	    	if(Arrays.asList(deck).indexOf(Card1)>Arrays.asList(deck).indexOf(Card2))
+	    		return deck[Arrays.asList(deck).indexOf(Card1)];
 	    	else
-	    		return dech[Arrays.asList(dech).indexOf(Card2)];
+	    		return deck[Arrays.asList(deck).indexOf(Card2)];
 	    
+	    }
+	    public int[] cardToRank(){
+	    	int ranks[] = new int[10];
+	    	ranks[0]=Arrays.asList(deck).indexOf(Card1);
+	    	ranks[1]=Arrays.asList(deck).indexOf(Card2);
+	    	return ranks;
+	    }
+	    public int s(int[] straight){
+	    	int c = 0;
+	    	Arrays.sort(straight);
+	    	for(int i=0;i<1;i++){
+	    		if(straight[i]==straight[i+1]-1)
+	    			c++;
+	    	}
+	    	if(c==1)
+	    		return 1;
+	    	return 0;
 	    }
 
 }
